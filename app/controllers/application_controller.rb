13f7e633a1735fa093b31bcad4910fa59c05c882
Zack <168221270+Zval321@users.noreply.github.com>
2024-11-19 21:26:08 -0600

    updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception, unless: -> { request.format.json? }
 
   def index
-    render file: Rails.root.join('public', 'dist', 'index.html')
+    render file: Rails.root.join('public', 'index.html')
   end
 
   def current_user
