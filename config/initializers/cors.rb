2def068e9f66f666df353b56ebeebbfa9c8c97a4
Zack <168221270+Zval321@users.noreply.github.com>
2024-11-20 09:53:09 -0600

    added updates to the cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'http://localhost:5173'  
+    origins 'http://localhost:3000', 
+            'http://localhost:5173', 
+            'https://capstone-to-do-frontend-2t2p281in-zackvalavanis-projects.vercel.app'
 
     resource '*',
       headers: :any,
